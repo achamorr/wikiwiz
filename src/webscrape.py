5074099f0047822231a44dc3fae6d71221507a91
achamorr <andrea.colleges@gmail.com>
2019-09-07 17:50:59 -0400

    almost getting Google cloud nlp api to work

--- src/webscrape.py
+++ src/webscrape.py
@@ -8,6 +8,24 @@
 
 from bs4 import BeautifulSoup
 
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+
+#===GLOBAL VARIABLES==
+HTML_TEST = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
 def isLegalType(word):
     # @todo: dictionary check?
 
@@ -22,15 +40,67 @@ def getArrayOfOrderedWords(htmlString):
     htmlArray = [word for word in htmlArray if isLegalType(word)]
     return htmlArray
 
-def parseQueryTermsFromHTMLContent(HTMLContent):
-    soup = BeautifulSoup(html_doc, HTMLContent)
+def tokenizeArrayOfWords(wordArray):
+    pass
+
+def initGoogleClientText():
+    client = language_v1.LanguageServiceClient()
+
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    #autodetect language
+    # @todo: allow this for several different languages :)
+
+    document = {"content": wordString,
+                "type": type_
+                }
+    
+    encoding_type = enums.EncodingType.UTF8
+
+    return client, document, encoding_type
+    
+
+
+def getEntitiesFromString(wordString):
+    #init
+    client, document, encoding_type = initGoogleClientText()
+
+    response = client.analyze_entities(document, encoding_type = encoding_type)
+    
+    #entitiesArray = [entity.name for entity in response.entities]
+    typesDict = {entity.name:entity.type for entity in response.entities}
+    metadataDict = entity.metadata
+
+    return typesDict, metadataDict
+
+def getContentClassification(wordString):
+    client, document, encoding_type = initGoogleClientText()
+
+    response = client.classify_text(document)
+
+    categoriesWithConfidenceList = [(category.name, category.confidence) for category in response.categories]
+
+def parseQueryTermsFromHTMLContent(htmlString):
+    soup = BeautifulSoup(htmlString, "html.parser")
     pageText = soup.get_text()
 
     queryTerms = []
+    contentClassification = None
 
     #split and filter
-    queryTerms = getArrayOfOrderedWords(pageText)
+    wordArray = getArrayOfOrderedWords(pageText)
         
     #tokenize
-    queryTerms = tokenizeArrayofWords(wordArray)
+    queryTypesDict, queryMetaDict = getEntitiesFromString(pageText)
 
+    #content classification, for good times
+    categoriesWithConfidenceList = getContentClassification(pageText)
+
+    return queryTypeDict, queryMetaDict, categoriesWithConfidenceList
+
+
+if __name__ == "__main__":
+    queryTypeDict, queryMetaDict, categoriesWithConfidenceList = parseQueryTermsFromHTMLContent(HTML_TEST)
+
+    print (HTML_TEST)
+    print (queryTypeDict, queryMetaDict, categoriesWithConfidenceList)
