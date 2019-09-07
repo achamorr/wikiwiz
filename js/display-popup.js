93a31fde66150f3e486bc418e6f93c5ad0012407
achamorr <andrea.colleges@gmail.com>
2019-09-07 14:32:41 -0400

    added display-popup.js

---
+++ js/display-popup.js
@@ -0,0 +1,23 @@
+// =======================================================
+// File: wikipedia-rest-search.py
+// Description:  Search Wikimedia REST API for terms
+// Created: 09.07.2019
+// Updated: 
+// Author: Andrea Chamorro
+// =======================================================
+
+
+"background": {
+    "scripts": ["background.js"],
+    "persistent": false
+  },
+  "options_page": "options.html",
+  "page_action": {
+      "default_popup": "popup.html",
+      "default_icon": {
+          "16": "images/get_started16.png",
+          "32": "images/get_started32.png",
+          "48": "images/get_started48.png",
+          "128": "images/get_started128.png"
+        }
+    },
