7eed328741c8f5f1510b56e81952b898650d6ee0
achamorr <andrea.colleges@gmail.com>
2019-09-07 21:27:25 -0400

    fixed manifest

---
+++ js/highlight-parse.js
@@ -0,0 +1,8 @@
+// =======================================================
+// File: highlight-parse.py
+// Description:  Get queryTerms from webpage via user highlight action.
+// Created: 09.07.2019
+// Updated: 
+// Author: Andrea Chamorro
+// =======================================================
+
